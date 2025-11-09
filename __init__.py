9dc949aa33a3b673bac98faa40c0e659abbbe034
Shaurya Khetarpal <shauryakhetarpal@Shauryas-MacBook-Air.local>
2025-11-09 19:27:44 +0530

    fixing init.py

--- __init__.py
+++ __init__.py
@@ -1,9 +1,9 @@
-from base_layers import ConvLayer, InvertedResidualBlock
+from .base_layers import ConvLayer, InvertedResidualBlock
 #from MobileViT_v1 import multihead_self_attention_2D
-from mobile_vit_v1_block import Transformer, MobileViT_v1_Block
+from .mobile_vit_v1_block import Transformer, MobileViT_v1_Block
 
-from configs import (
+from .configs import (
     Config_MobileViT_v1_XXS,
     get_mobile_vit_v1_configs,
 )
-from mobile_vit_v1 import MobileViT_v1, build_MobileViT_v1
+from .mobile_vit_v1 import MobileViT_v1, build_MobileViT_v1
